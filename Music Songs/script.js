0a94f1186f5bc37ed42ff540fb3bab328727965a
Jatin Grover <groverjatin4444@gmail.com>
2022-05-28 20:28:13 +0530

    Initial commit

---
+++ Music Songs/script.js
@@ -0,0 +1,109 @@
+console.log("Hi");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "Sadi Gali - Wedding Release", filePath: "songs/1.mp3", coverPath: "music photos/11.jpg"},
+    {songName: "Electronomia- Sky High", filePath: "songs/2.mp3", coverPath: "music photos/12.png"},
+    {songName: "Electronomia - remix", filePath: "songs/3.mp3", coverPath: "music photos/13.png"},
+    {songName: "Electronomia - NCS", filePath: "songs/4.mp3", coverPath: "music photos/14.jpg"},
+    {songName: "Jarico - Island", filePath: "songs/5.mp3", coverPath: "music photos/15.jpg"},
+    {songName: "NCS - SuperRemix", filePath: "songs/6.mp3", coverPath: "music photos/16.jpg"},
+    {songName: "Prophec Mashup - Tu Hi Aah", filePath: "songs/7.mp3", coverPath: "music photos/17.jpg"},
+    {songName: "Prophec Mashup - Remix OP", filePath: "songs/8.mp3", coverPath: "music photos/18.jpg"},
+    {songName: "Prophec - Wedding Mashup", filePath: "songs/9.mp3", coverPath: "music photos/19.jpg"},
+    {songName: "Prophec Mashup - Close Song", filePath: "songs/10.mp3", coverPath: "music photos/20.jpg"},
+]
+
+songItems.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+ 
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
---
+++ js/main2.js
@@ -0,0 +1,107 @@
+const video = document.getElementById("video");
+const isScreenSmall = window.matchMedia("(max-width: 700px)");
+let predictedAges = [];
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  faceapi.nets.ageGenderNet.loadFromUri("/models")
+]).then(startVideo);
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => (video.srcObject = stream),
+    err => console.error(err)
+  );
+}
+function screenResize(isScreenSmall) {
+  if (isScreenSmall.matches) {
+    // If media query matches
+    video.style.width = "320px";
+  } else {
+    video.style.width = "500px";
+  }
+}
+
+screenResize(isScreenSmall); // Call listener function at run time
+isScreenSmall.addListener(screenResize);
+
+video.addEventListener("playing", () => {
+  console.log("playing called");
+  const canvas = faceapi.createCanvasFromMedia(video);
+  let container = document.querySelector(".container");
+  container.append(canvas);
+
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectSingleFace(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions()
+      .withAgeAndGender();
+
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    console.log(resizedDetections);
+
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    if (resizedDetections && Object.keys(resizedDetections).length > 0) {
+      const age = resizedDetections.age;
+      const interpolatedAge = interpolateAgePredictions(age);
+      const gender = resizedDetections.gender;
+      const expressions = resizedDetections.expressions;
+      const maxValue = Math.max(...Object.values(expressions));
+      const emotion = Object.keys(expressions).filter(
+        item => expressions[item] === maxValue
+      );
+      document.getElementById("age").innerText = `Age - ${interpolatedAge}`;
+      document.getElementById("gender").innerText = `Gender - ${gender}`;
+      document.getElementById("emotion").innerText = `Emotion - ${emotion[0]}`;
+      document.getElementById("percentage").innerText = `Recognising Scale - ${maxValue}`;
+          var percentage=(`${maxValue}`)
+          /*setInterval(()=>{
+  
+          if(percentage>0.80){
+            alert("Recognised")
+
+          }
+        },1000
+          )*/
+      var input = `${emotion[0]}`;
+      console.log(input);
+      setTimeout(() =>{
+        if(input == "sad"){
+          window.location.href ="http://127.0.0.1:5501/explore%20options/explore.html#sad"
+        }
+        else if(input == "neutral"){
+          window.location.href ="http://127.0.0.1:5501/explore%20options/explore.html#games"
+        }
+        else if(input == "happy"){
+          window.location.href ="http://127.0.0.1:5501/explore%20options/explore.html#happy"
+        }
+        else if(input == "angry"){
+          window.location.href ="http://127.0.0.1:5501/explore%20options/explore.html#sad"
+        }
+        else if(input == "disgusted"){
+          window.location.href ="http://127.0.0.1:5501/explore%20options/explore.html#sad"
+        }
+  
+
+      },3000)
+          }
+  }, 10);
+});
+
+function interpolateAgePredictions(age) {
+  predictedAges = [age].concat(predictedAges).slice(0, 30);
+  const avgPredictedAge =
+    predictedAges.reduce((total, a) => total + a) / predictedAges.length;
+  return avgPredictedAge;
+}
