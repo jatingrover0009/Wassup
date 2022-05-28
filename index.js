0e5dcefb8cfa3d1c5615e8a72a4583fda7690fb9
JatinGrover <groverjatin4444@gmail.com>
2022-05-29 02:47:45 +0530

    adding live server

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const http = require('http')
-const fs = require('fs')
+//const http = require('http')
+//const fs = require('fs')
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('./front page/index.html').pipe(res)
-})
+//const server = http.createServer((req, res) => {
+//  res.writeHead(200, { 'content-type': 'text/html' })
+//  fs.createReadStream('./front page/index.html').pipe(res)
+// })
 
-server.listen(process.env.PORT || 3000);
+//server.listen(process.env.PORT || 3000);
+var params = {
+	port: 8181, // Set the server port. Defaults to 8080.
+	host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+	root: "./", // Set root directory that's being served. Defaults to cwd.
+	open: false, // When false, it won't load your browser by default.
+	ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+	file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+	wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+	mount: [['/components', './node_modules']], // Mount a directory to a route.
+	logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+	middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+};
+liveServer.start(params);
